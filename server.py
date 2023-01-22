c84d2f4d87edcba46c9aa7fc8ef57992e6220230
jaskim9824 <jaskim9824@gmail.com>
2023-01-22 12:52:37 -0700

    base function done, need to test on own and clean up

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2023 Jason Kim
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,6 +28,12 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# Sources:
+# https://www.geeksforgeeks.org/python-check-if-a-file-or-directory-exists/
+# https://uofa-cmput404.github.io/cmput404-slides/04-HTTP
+# https://www.w3.org/Protocols/HTTP/1.0/spec.html
+# https://tecfa.unige.ch/moo/book2/node93.html
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
@@ -35,24 +43,93 @@ class MyWebServer(socketserver.BaseRequestHandler):
         requestString = requestString[2:requestString.find("HTTP")]
         print ("Got a request of: %s\n" % requestString)
         requestArray = requestString.split()
+        response = ""
         if requestArray[0] == "GET":
-            self.sendGetResponse(requestArray[1])
+            response = self.returnResponse(requestArray[1])
         else:
             print("Requests other than GET are not supported")
-        self.request.sendall(bytearray("OK",'utf-8'))
+            response = "HTTP/1.1 405 Method Not Allowed\r\n"
+        print(response)
+        self.request.sendall(bytearray(response,'utf-8'))
 
-    def sendGetResponse(self, requestedPath):
+    def serveFileRequest(self, pathToFile, fileType):
+        response = """HTTP/1.1 200 OK \r\nContent-type: {contentType}\r\nConnection:keep-alive\r\n\r\n"""
+        if fileType == ".html":
+            response = response.format(contentType="text/html")
+            file = open(pathToFile, "r")
+            response += file.read()
+            file.close()
+            return response
+        elif fileType == ".css":
+            response = response.format(contentType="text/css")
+            file = open(pathToFile, "r")
+            response += file.read()
+            file.close()
+            return response
+        return ""
+
+
+    def returnResponse(self, requestedPath):
         print("Handling GET request for route " + requestedPath)
         pathArray = requestedPath.split("/")
-        #requested index
+        print(pathArray)
+        self.cleanPathArray(pathArray)
+        print(pathArray)
+        depth = self.checkPathDepth(pathArray)
+        print("Handling path at path depth: " + str(depth))
+        if (depth < 0):
+            print("Attempting to access path outside of www directory, forbidden")
+            return "HTTP/1.1 404 Not Found \r\n"
         if (pathArray[-1] == ""):
             print("Requested Directory, returning ./www" + requestedPath +"index.html")
-        elif (not pathArray[-1].find(".")):
-            print("Redirect to path " + requestedPath +"/")      
+            if not os.path.exists("./www" + requestedPath + "index.html"):
+                return "HTTP/1.1 404 Not Found \r\n"
+            else:
+                return self.serveFileRequest("./www" + requestedPath + "index.html", ".html")
+        elif (pathArray[-1].find(".html") == -1 and pathArray[-1].find(".css") == -1):
+            print("Redirect to path " + requestedPath +"/")   
+            if not os.path.exists("./www" + requestedPath + "/" + "index.html"):
+                return "HTTP/1.1 404 Not Found \r\n"
+            else:
+                return "HTTP/1.1 301 Moved Permanently\r\nLocation:http://127.0.0.1:8080" + requestedPath + "/"   
         #requested file
         else:
-            print("Requesting file ./www" + requestedPath)
+            print("Requesting file www" + requestedPath)
+            fileExt = ""
+            if pathArray[-1].find(".html") != -1:
+                fileExt = ".html"
+            elif pathArray[-1].find(".css") != -1:
+                fileExt = ".css"
+            if not os.path.exists("./www" + requestedPath):
+                return "HTTP/1.1 404 Not Found  \r\n"
+            else:
+                return self.serveFileRequest("./www" + requestedPath, fileExt)
 
+    def cleanPathArray(self, pathArray):
+        counter = 0
+        for i in range(0, len(pathArray)-1):
+            if pathArray[i] == "":
+                counter += 1
+        while counter > 0:
+            pathArray.remove("")
+            counter -= 1
+
+    def checkPathDepth(self, pathArray):
+        # depth = 0, www directory
+        depth = 0
+        for pathItem in pathArray:
+            print(pathItem)
+            if pathItem == "" or pathItem.find(".html") != -1 or pathItem.find(".css") != -1:
+                print("Stopping")
+                return depth
+            elif pathItem == "..":
+                print("Reducing depth")
+                depth -= 1
+            else:
+                print("Increasing depth")
+                depth += 1
+        print(depth)
+        return depth
 
 
 
